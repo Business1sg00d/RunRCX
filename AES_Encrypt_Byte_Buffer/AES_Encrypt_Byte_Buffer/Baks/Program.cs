360e4be9a52c3870c2d4c80aa6bc6b4dc51cadfd
Business1sg00d <Business1sg00d@proton.me>
2025-03-02 15:08:08 -0500

    Add AES_ENCRYPT repo

---
+++ AES_Encrypt_Byte_Buffer/AES_Encrypt_Byte_Buffer/Baks/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.IO;
+using System.Collections.Generic;
+using System.IO.Compression;
+
+namespace AES_Encrypt_Byte_Buffer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] baconFile;
+            byte[] plaintextbuf;
+            byte[] compressedBuff;
+            byte[] encryptedData;
+            string[] hexValues;
+
+            // Option #1 is for a beacon. Option #2 is for a stager in byte[] format without brackets; I.e. "0x00,0x01,0x03...."
+            if (args.Length < 1) {
+                Console.WriteLine("Usage: .\\AES_Encrypt_Byte_Buffer.exe beaconBinary.exe 1");
+                Console.WriteLine("Usage: .\\AES_Encrypt_Byte_Buffer.exe [file Conaining payload in byte[] format] 2");
+                return;
+            }
+
+            if (args[1] == "1")
+            {
+                // Read in file as bytes:
+                baconFile = System.IO.File.ReadAllBytes(args[0]);
+
+                // GZIP data:
+                compressedBuff = compressGzip(baconFile);
+
+                // Encrypt gzip data:
+                encryptedData = encFunc(compressedBuff);
+
+                // Base64 Encode encryptedData, then write to a txt file:
+                string b64 = Convert.ToBase64String(encryptedData);
+                string currentDir = Directory.GetCurrentDirectory();
+                string filePath = currentDir + "/" + "b64Output.txt";
+                File.WriteAllText(filePath, b64);
+
+            }
+            else if (args[1] == "2")
+            {
+                // Read in file:
+                hexValues = File.ReadAllText(args[0]).Split(',');
+                
+                // Allocate byte array and parse input file:
+                plaintextbuf = parseCommas(hexValues);
+
+                // GZIP data
+                compressedBuff = compressGzip(plaintextbuf);
+
+                // Encrypt gzip data:
+                encryptedData = encFunc(compressedBuff);
+
+                // Base64 Encode encryptedData, then write to a txt file:
+                string b64 = Convert.ToBase64String(encryptedData);
+                string currentDir = Directory.GetCurrentDirectory();
+                string filePath = currentDir + "/" + "b64Output.txt";
+                File.WriteAllText(filePath, b64);
+            }
+        }
+
+        private static byte[] compressGzip(byte[] Data)
+        {
+            using (var freshStream = new MemoryStream())
+            using (var gzipStream = new GZipStream(freshStream, CompressionMode.Compress))
+            {
+                gzipStream.Write(Data, 0, Data.Length);
+                return freshStream.ToArray();
+            }
+        }
+
+        static byte[] parseCommas(string[] hexValues)
+        {
+            byte[] plaintextbuf = new byte[hexValues.Length];
+
+            // Parse each "0xNN" value
+            for (int i = 0; i < hexValues.Length; i++)
+            {
+                string hex = hexValues[i].Trim(); // Remove any extra whitespace
+                plaintextbuf[i] = Convert.ToByte(hex, 16); // Convert hex to byte
+                
+            }
+            return plaintextbuf;
+        }
+
+        static byte[] encFunc(byte[] plaintextbuf) {
+
+            // Encrypt shellcode.
+            Aes aes = Aes.Create();
+            byte[] key = new byte[16] { 0x64, 0x26, 0x16, 0x83, 0xa9, 0x5d, 0xcf, 0xc6, 0xa8, 0x4c, 0x75, 0xf1, 0x42, 0x3a, 0x1c, 0x88 };
+            byte[] iv = new byte[16] { 0x5b, 0xb8, 0x47, 0x7f, 0xc0, 0x83, 0x10, 0xc9, 0x2b, 0x83, 0xec, 0x3f, 0x16, 0x1b, 0x6f, 0x7f };
+            ICryptoTransform encrypter = aes.CreateEncryptor(key, iv);
+            byte[] buf;
+            using (var msEncrypt = new System.IO.MemoryStream(plaintextbuf))
+            {
+                using (var csEncrypt = new CryptoStream(msEncrypt, encrypter, CryptoStreamMode.Read))
+                {
+                    using (var msPlain = new System.IO.MemoryStream())
+                    {
+                        csEncrypt.CopyTo(msPlain);
+                        buf = msPlain.ToArray();
+                    }
+                }
+            }
+            return buf;
+        }
+    }
+}
---
+++ AES_Encrypt_Byte_Buffer/AES_Encrypt_Byte_Buffer/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AES_Encrypt_Byte_Buffer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AES_Encrypt_Byte_Buffer")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("61607cf4-e872-41c0-989b-0c2c3cfa508a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
